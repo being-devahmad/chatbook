8974f7127bb74bf79b223f3420415aef87fb5073
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-21 01:53:10 +0500

    signup functionality implemented from client side

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -23,5 +23,22 @@ export const useAuthStore = create((set) => ({
                 isCheckingAuth: false
             })
         }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({
+                authUser: res.data
+            });
+            toast.success("Account created successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({
+                isSigningUp: false
+            });
+        }
     }
 }))
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express' // type -> module
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import { connectDB } from "./lib/db.js"
 
@@ -14,6 +15,10 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser()) //  allows you to parse the cookies just like jwt token
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
