544c67159a136bd228f48b0ad7a1d2d69b9c3b87
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-20 17:30:35 +0500

    basic mvc setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,10 @@
 import express from 'express' // type -> module
 const app = express()
 
+import authRoutes from "./routes/auth.route.js"
+
+app.use('/api/auth' , authRoutes )
+
 app.listen(5001, () => {
     console.log("server is running on port 5001")
 })
