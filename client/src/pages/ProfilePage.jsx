c7f13d4057d1801f0703f5176b4b81f4d84bab48
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-20 22:41:34 +0500

    client side routing and signup page commit
    zustand store done

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
+
+const ProfilePage = () => {
+
+  const { authUser } = useAuthStore()
+
+
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
+
+export default ProfilePage
---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,27 @@
+import { create } from "zustand"
+import { axiosInstance } from "../lib/axios"
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const response = await axiosInstance.get('/auth/check')
+            set({
+                authUser: response.data
+            })
+        } catch (error) {
+            console.log("Erorr in checkAuth : ", error)
+            set({
+                authUser: null
+            })
+        } finally {
+            set({
+                isCheckingAuth: false
+            })
+        }
+    }
+}))
