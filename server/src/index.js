604b1bc34c3ad7e406fa10285b78f221be5ac455
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-20 21:37:44 +0500

    message routes and logic commit

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const app = express()
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(cookieParser()) //  allows you to parse the cookies just like jwt token
 
 app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
---
+++ server/src/routes/message.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers, getMessages, sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+
+router.get("/users", protectedRoute, getAllUsers)
+router.get("/:id", protectedRoute, getMessages)
+
+router.post("/send/:id", protectedRoute, sendMessage);
+
+export default router;
