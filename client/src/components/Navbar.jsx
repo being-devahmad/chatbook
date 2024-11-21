53b6179b191e9c87d472c7f784d4eb6e75d85d66
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-22 03:50:53 +0500

    project images commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
 
 const Navbar = () => {
-    const { authUser , logout } = useAuthStore();
+    const { authUser, logout } = useAuthStore();
 
     return (
         <header
@@ -17,7 +17,7 @@ const Navbar = () => {
                             <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                                 <MessageSquare className="w-5 h-5 text-primary" />
                             </div>
-                            <h1 className="text-lg font-bold">Chatty</h1>
+                            <h1 className="text-lg font-bold">ChatBoook</h1>
                         </Link>
                     </div>
 
